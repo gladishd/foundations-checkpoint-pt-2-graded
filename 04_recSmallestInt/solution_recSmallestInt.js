925c93504f416087ced2c1cf473ba21e2f643377
ScottDAlessandro <sdaless22@gmail.com>
2018-08-14 18:06:23 -0400

    initial commit

---
+++ 04_recSmallestInt/solution_recSmallestInt.js
@@ -0,0 +1,11 @@
+/* 
+  DO NOT refactor the function declaration to a function expression using
+  `const` or `let`, the test specs won't be able to test the function properly.
+  
+*/
+
+function recSmallestInt(arr) {
+  // write code here
+
+}
+
